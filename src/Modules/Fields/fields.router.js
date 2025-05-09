f8f80db97e3b3c9b36ed4bf8c7ae0589f912e97a
Mostafaamin1000 <mostafaamin09098@mail.com>
2025-05-09 07:59:00 -0700

    Set field Router

--- src/Modules/Fields/fields.router.js
+++ src/Modules/Fields/fields.router.js
@@ -1,7 +1,12 @@
 import {Router} from 'express';
+import { addField, deleteField, getAllFields, getFieldById, updateField } from './fields.controller.js';
 
 const fieldRouter = Router(); 
-
+fieldRouter.post('/create',addField)
+fieldRouter.get('/allfields',getAllFields)
+fieldRouter.get('/:id',getFieldById)
+fieldRouter.put('/update/:id',updateField)
+fieldRouter.delete('/delete/:id',deleteField)
 
 
 
--- src/Modules/bootstrap.js
+++ src/Modules/bootstrap.js
@@ -1,6 +1,8 @@
 import authRouter from "./auth/auth.router.js"
+import fieldRouter from "./Fields/fields.router.js"
 
 
 export const bootstrap =(app)=>{
 app.use('/api/auth',authRouter)
+app.use('/api/field',fieldRouter)
 }
