8e2a164304a2ed179c5fca2a7222e7fde4cbf731
Mostafaamin1000 <mostafaamin09098@mail.com>
2025-05-12 11:44:17 -0700

    Change url

--- DB/db.connection.js
+++ DB/db.connection.js
@@ -1,8 +1,8 @@
 import mongoose  from "mongoose";    
 import dotenv from "dotenv";
 dotenv.config();
-export const dbConnection = async()=>{
-    await mongoose.connect(process.env.MONGO_URI)
+export const dbConnection =()=>{
+ mongoose.connect(process.env.MONGO_URI)
 .then(() => {console.log('connected to database');
 })
 .catch((err) => {console.log(err.message)});
