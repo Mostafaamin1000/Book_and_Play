974366703024c41cb934e7f6867ab9a4cf10d1c8
Mostafaamin1000 <mostafaamin09098@mail.com>
2025-05-20 08:11:01 -0700

    try someting

--- src/Modules/Match/match.router.js
+++ src/Modules/Match/match.router.js
@@ -5,12 +5,15 @@ import { allowTo, protectedRouter } from '../auth/auth.controller.js';
 const matchRouter = Router();
 matchRouter.post('/create',protectedRouter, allowTo('player', 'owner') ,addMatch);
 matchRouter.post('/:id/join',protectedRouter, allowTo('player') , joinMatch);
-matchRouter.get('/:userId', getUserMatches);
+
 matchRouter.get('/available', getAvailableMatches);
+matchRouter.get('/:userId', getUserMatches);
 matchRouter.get('/allmatches', getMatches);
 matchRouter.get('/:id', getMatchbyId);
+
 matchRouter.put('/update/:id', updateMatch);
-matchRouter.delete('/:id/unjoin',protectedRouter, allowTo('player') , unjoinMatch);
+
 matchRouter.delete('/delete/:id', deleteMatch);
+matchRouter.delete('/:id/unjoin',protectedRouter, allowTo('player') , unjoinMatch);
 
 export default matchRouter;
